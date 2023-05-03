de75b957cd8680471eafc0bcf19e9274817eec0d
anikaaurony <aurony111@gmail.com>
2023-05-04 03:10:46 +0600

    done

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/', (req, res) => {
 app.get('/chefs', (req, res) => {
     res.send(chefs)
   })
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chefs.find(n => n._id === id);
+    res.send(selectedChef)
+})
   
   app.listen(port, () => {
     console.log(`This is running on ${port}`)
